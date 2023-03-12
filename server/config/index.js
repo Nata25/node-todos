eab8380a0172777bb822ffb01935c484f1e1616e
Natalya Ivanova <natalyas.ivanova@gmail.com>
2023-03-12 15:11:21 +0200

    Move db connect configuration to .env file

--- server/config/index.js
+++ server/config/index.js
@@ -1,9 +1,11 @@
-const configValues = require('./config.json');
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+const dbName = process.env.DB_NAME;
 
 module.exports = {
   getDbConnectionParams: function() {
-    const str = `mongodb+srv://${configValues.uname}:${configValues.pwd}@cluster0.wh9zu9v.mongodb.net/?retryWrites=true&w=majority`;
-    const params =  { dbName: configValues['db-name'] };
+    const str = `mongodb+srv://${username}:${password}@cluster0.wh9zu9v.mongodb.net/?retryWrites=true&w=majority`;
+    const params =  { dbName };
     return [str, params];
   }
 }
